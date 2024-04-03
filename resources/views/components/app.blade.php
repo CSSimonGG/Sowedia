64981632d0485af300d02bbf2a93b736fe49fb11
Simon <satmdeklerk@gmail.com>
2024-04-03 15:29:56 +0200

    Favicon

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -2,11 +2,18 @@
 <html lang="{{ __('lang.lang') }}">
 
 <head>
+    <!-- Meta -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sowedia | {{ __('lang.title') }}</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('imgs/favicon.png') }}">
+
+    <!-- CSS -->
     @vite('resources/css/app.css')
+
+    <title>Sowedia | {{ __('lang.title') }}</title>
 </head>
 
 <body class="font-inter">
