2b948586fe954908351f4197370e887d3328b715
Simon <satmdeklerk@gmail.com>
2024-04-12 22:37:36 +0200

    Reviews component fix

--- resources/views/components/reviews.blade.php
+++ resources/views/components/reviews.blade.php
@@ -1,32 +1,34 @@
 <div class="bg-blue text-white w-full min-h-[420px]">
-    <div class="xl:ml-20 max-xl:mx-10 xl:flex pb-16">
-        <div class="2xl:w-1/3 pt-16">
+    <div class="xl:ml-20 max-xl:mx-10 flex pb-16 max-xs:flex-wrap">
+        <div class="2xl:w-1/3 min-w-[160px] pt-16">
             <div class="">
                 <img src="{{ asset('imgs/sowedia_logo.png') }}" alt="Sowedia" class="w-[150px]">
             </div>
             <p class="text-gray text-sm mt-4 ml-1">There are no reviews yet</p>
             <p class="ml-1">Powered by Google Reviews</p>
         </div>
-        <div class="flex space-x-4 xl:mt-[75px] max-xl:mt-10 max-2xl:xl:ml-20 max-xs:hidden">
-            <div class="mt-[112px]">
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 26.5604L11.3066 17.8671C10.28 16.8404 10.28 15.1604 11.3066 14.1338L20 5.44043"
-                        stroke="#F8FBFD" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                </svg>
-            </div>
-            <div class="bg-white h-[270px] w-[227px]"></div>
-            <div class="bg-white h-[270px] w-[227px] max-sm:hidden"></div>
-            <div class="bg-white h-[270px] w-[227px] max-2md:hidden"></div>
-            <div class="mt-[112px]">
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M11.8799 26.5604L20.5732 17.8671C21.5999 16.8404 21.5999 15.1604 20.5732 14.1338L11.8799 5.44043"
-                        stroke="#F8FBFD" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                </svg>
+        <div class="overflow-auto">
+            <div class="flex space-x-4 xl:mt-[75px] max-xl:mt-10 max-2xl:xl:ml-20 w-[809px]">
+                <div class="mt-[112px] max-xl:hidden">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20 26.5604L11.3066 17.8671C10.28 16.8404 10.28 15.1604 11.3066 14.1338L20 5.44043"
+                            stroke="#F8FBFD" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                    </svg>
+                </div>
+                <div class="bg-white h-[270px] w-[227px]"></div>
+                <div class="bg-white h-[270px] w-[227px]"></div>
+                <div class="bg-white h-[270px] w-[227px]"></div>
+                <div class="mt-[112px] max-xl:hidden">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M11.8799 26.5604L20.5732 17.8671C21.5999 16.8404 21.5999 15.1604 20.5732 14.1338L11.8799 5.44043"
+                            stroke="#F8FBFD" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                    </svg>
+                </div>
             </div>
         </div>
     </div>
